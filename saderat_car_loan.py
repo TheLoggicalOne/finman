31690e5b5eb146dd6a5dd0c28e5b2542a612b0fa
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2023-12-26 15:18:47 +0330

    first commit after deletion of .idea folder and solving the brown background of project window

--- saderat_car_loan.py
+++ saderat_car_loan.py
@@ -25,6 +25,31 @@ x = pv_calc.pv_calc(payments=cash_flow, discount_rate=r_real)
 
 # test
 
+class Loan:
+    def __init__(self, present_value=1, interest_rate=0, number_of_periods=1):
+        self.pv = present_value
+        self.ir = interest_rate
+        self.n_periods = number_of_periods
+        self.periodic_payment = pv_calc.periodic_payment_calc(pv=self.pv, n_periods=self.n_periods,
+                                                              discount_rate=self.ir)
+
+    def to_settle_payment(self, n_preiodic_payments_made, time_passed=None):
+        if time_passed is None:
+            time_passed = n_preiodic_payments_made
+
+        pv_of_paid = pv_calc.periodic_pv_calc(self.periodic_payment, n_preiodic_payments_made, self.ir)
+        pv_of_to_pay = self.pv - pv_of_paid
+        pv_of_to_pay_today = pv_of_to_pay * (1+self.ir) ** time_passed
+        return pv_of_to_pay_today
+
+
+if __name__ == '__main__':
+    sd_loan = Loan(present_value=pv, interest_rate=r_real, number_of_periods=n_periods)
+    x = sd_loan.to_settle_payment(4, 5.33)
+    y = sd_loan.to_settle_payment(4, 4)
+
+
+
 
 
 
